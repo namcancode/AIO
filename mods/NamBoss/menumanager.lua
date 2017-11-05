262f28e62a822fffe5e37992f5c1592ca2a12cbc
NamBoss <namhbnam@users.noreply.github.com>
2017-11-05 12:32:04 +0700

    up

--- mods/NamBoss/menumanager.lua
+++ mods/NamBoss/menumanager.lua
@@ -6,5 +6,5 @@ Hooks:Add("MenuManagerBuildCustomMenus", "MenuManagerBuildCustomMenus_SIIL", fun
     MenuHelper:AddMenuItem( nodes.lobby, "steam_inventory", "menu_steam_inventory", "menu_steam_inventory_help", "inventory", "after" )
     MenuHelper:AddMenuItem( nodes.crime_spree_lobby, "steam_inventory", "menu_steam_inventory", "menu_steam_inventory_help", "inventory", "after" )
 end)
-Announcer:AddHostMod("Truy cap http://mod.nguyennam.me de down them mod")
-Announcer:AddClientMod("Dont kick me pls!!!\nTruy cap http://mod.nguyennam.me de down them mod.")
+--Announcer:AddHostMod("Truy cap http://mod.nguyennam.me de down them mod")
+--Announcer:AddClientMod("Dont kick me pls!!!\nTruy cap http://mod.nguyennam.me de down them mod.")
