5de13fd524373081de391d48740ac0d2beeba68b
NamBoss <namhbnam@users.noreply.github.com>
2018-01-15 21:17:54 +0700

    update superblt

--- mods/Tong Hop/level.lua
+++ mods/Tong Hop/level.lua
@@ -1,5 +1,5 @@
-managers.experience:_set_current_level(99)
+managers.experience:_set_current_level(90)
 -- Message on screen
 if managers.hud then
-   managers.hud:show_hint( { text = "Ban Da Dat level 99 - Duoc code boi Nam AK - http://nguyennam.ml"} )
+   managers.hud:show_hint( { text = "Ban Da Dat level 90 - Duoc code boi Nam AK - http://nguyennam.ml"} )
 end
