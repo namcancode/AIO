20bc0762fcca65243555927acbacdd11d332b094
NamBoss <namhbnam@users.noreply.github.com>
2018-03-11 11:58:34 +0700

    up

--- mods/Tong Hop/fixfps.lua
+++ mods/Tong Hop/fixfps.lua
@@ -1,39 +1,28 @@
 SetVideo = false
-
-
-    	for _, vp in ipairs(managers.viewport:viewports()) do
-		--vp:vp():set_post_processor_effect("World", Idstring("hdr_post_processor"), Idstring("empty"))--
+for _, vp in ipairs(managers.viewport:viewports()) do
+		
 		vp:vp():set_post_processor_effect("World", Idstring("post_SSAO"), Idstring("empty"))
-    -- vp:vp():set_post_processor_effect("World", Idstring("bloom_combine_post_processor"), Idstring("bloom_combine_empty"))--
-  --- vp:vp():set_post_processor_effect("World", Idstring("bloom_combine"), Idstring("bloom_combine_empty"))-- out light
-    
-		--vp:vp():set_post_processor_effect("World", Idstring("shadow_processor"), Idstring("empty"))  -- mat bong toi
-    vp:vp():set_post_processor_effect("World", Idstring("shadow_modifier"), Idstring("empty"))--
-     
+    vp:vp():set_post_processor_effect("World", Idstring("shadow_modifier"), Idstring("empty"))    
     vp:vp():set_post_processor_effect("World", Idstring("video_anti_alias"), Idstring("AA_off"))
-        vp:vp():set_post_processor_effect("World", Idstring("shadow_rendering"), Idstring("empty"))--
-		    vp:vp():set_post_processor_effect("World", Idstring("light_adaption"), Idstring("no_light_adaption"))
-vp:vp():set_post_processor_effect("World", Idstring("post_LEX_composite"), Idstring("empty"))
-vp:vp():set_post_processor_effect("World", Idstring("post_DOF"), Idstring("empty"))
-      
-       
-       
-        
-        
-      vp:vp():set_post_processor_effect("World", Idstring("depth_projection"), Idstring("depth_project_empty"))
-     vp:vp():set_post_processor_effect("World", Idstring("shadow_slice_depths"), Vector3(0, 0, 0))
-     vp:vp():set_post_processor_effect("World", Idstring("shadow_slice_overlap"), Vector3(0, 0, 0))
-      
-   vp:vp():set_post_processor_effect("World", Idstring("deferred_lighting"), Idstring("empty"))
-  vp:vp():set_post_processor_effect("World", Idstring("apply_ambient"), Idstring("empty"))
- -- vp:vp():set_post_processor_effect("World", Idstring("color_grading_post"), Idstring("empty"))   -lam mo man hinh
-  vp:vp():set_post_processor_effect("World", Idstring("reflection_visualization"), Idstring("empty"))
-  vp:vp():set_post_processor_effect("World", Idstring("glossiness_visualization"), Idstring("empty"))
-  vp:vp():set_post_processor_effect("World", Idstring("specular_visualization"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("shadow_rendering"), Idstring("empty"))
+		vp:vp():set_post_processor_effect("World", Idstring("light_adaption"), Idstring("no_light_adaption"))
+    vp:vp():set_post_processor_effect("World", Idstring("post_LEX_composite"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("post_DOF"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("depth_projection"), Idstring("depth_project_empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("shadow_slice_depths"), Vector3(0, 0, 0))
+    vp:vp():set_post_processor_effect("World", Idstring("shadow_slice_overlap"), Vector3(0, 0, 0))
+    vp:vp():set_post_processor_effect("World", Idstring("deferred_lighting"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("apply_ambient"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("reflection_visualization"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("glossiness_visualization"), Idstring("empty"))
+    vp:vp():set_post_processor_effect("World", Idstring("specular_visualization"), Idstring("empty"))
+   -- vp:vp():set_post_processor_effect("World", Idstring("color_grading_post"), Idstring("empty"))   --lam xam man hinh menu
+  -- vp:vp():set_post_processor_effect("World", Idstring("bloom_combine_post_processor"), Idstring("bloom_combine_empty"))--
+  --- vp:vp():set_post_processor_effect("World", Idstring("bloom_combine"), Idstring("bloom_combine_empty"))-- out light
+    --vp:vp():set_post_processor_effect("World", Idstring("hdr_post_processor"), Idstring("empty"))--
+    --vp:vp():set_post_processor_effect("World", Idstring("shadow_processor"), Idstring("empty"))  -- mat bong toi
 
 end
-
-
 	
 if Utils:IsInHeist() then
   SetVideo = true 
